ff82d58658b62c49e287b216ee53931eeea5c5bf
thanhkhon03 <2121050666@student.humg.edu.vn>
2023-05-23 16:48:19 +0700

    BTH235

--- BaiThucHanh2305/Slide7.java
+++ BaiThucHanh2305/Slide7.java
@@ -1,9 +1,8 @@
-package BaiThucHanh2305.Bai1;
+package BaiThucHanh2305;
 
 public class Slide7 {
     public static void main(String[] args) {
         try {
-            String s = null;
             System.out.println("Biến s là null.");
         } catch (Exception e) {
             System.out.println(e);
