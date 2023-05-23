db4c7a8ffe87232586392ed78ba60cdf86d24766
thanhkhon03 <2121050666@student.humg.edu.vn>
2023-05-23 07:20:05 +0700

    BTH 9-5

--- BaiThucHanh0905/Sets/HashSetsDemo.java
+++ BaiThucHanh0905/Sets/HashSetsDemo.java
@@ -3,9 +3,9 @@ package Sets;
 import java.util.HashSet;
 import java.util.Scanner;
 
-public class HashSetDemo {
+public class HashSetsDemo {
     public static void main(String[] args) {
-        HashSet<Student> students = new HashSet<>();
+        HashSet<student> student = new HashSet<>();
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Nhập số lượng sinh viên: ");
